a1eb825a769a3edb25929feb748b62c47d4d57da
Freya Watkinson <67429855+freyawatkinson@users.noreply.github.com>
2021-06-30 09:20:12 +1200

    Login page backend update

--- src/Login.js
+++ src/Login.js
@@ -3,8 +3,47 @@ import './Login.css';
 import Layout from './Layout';
 import placeholder from './placeholder.png';
 
-function Login() {
+import Axios from "Axios";
+
+function LoginPage() {
+
+    const [usernameReg, setUsernameReg] = useState("");
+    const [passwordReg, setPasswordReg] = useState("");
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [loginStatus, setLoginStatus] = useState("");
+
+    const register = () => {
+
+      Axios.post('http://localhost3002/register', {
+        username: usernameReg,
+        password: passwordReg,
+      }).then((response) => {
+        console.log(response);
+      }) ;
+    }
+
+    const login = () => {
+
+      Axios.post('http://localhost3002/login', {
+        username: username,
+        password: password,
+
+      }).then((response) => {
+        if (response.data.message) {
+          setLoginStatus(response.data.message)
+        } else {
+          setLoginStatus(response.data[0].username)
+        }
+      }) ;
+    }
+
     return (
+
+        <div className="Login">
+
         <Layout>
             <div className="phatBox">
 
@@ -12,7 +51,35 @@ function Login() {
 
             </div>
         </Layout>
+
+        <div className="registration">
+
+          <h1>Registration</h1>
+          <label>Username</label>
+
+          <input type="text" onChange={(e) => {
+            setUsernameReg(e.target.value)
+          }}></input>
+
+          <label>Password</label>
+
+          <input type="text"></input>
+          <button onClick={register}>Register</button>
+          
+        </div>
+
+        <div className="login">
+
+          <h1>Login</h1>
+          <input type="text" placeholder="Enter Username"></input>
+          <input type="text" placeholder="Enter Password"></input>
+          <button>Login</button>
+          
+        </div>
+
+        </div>
+
     );
 }
 
-export default Login;
+export default LoginPage;
